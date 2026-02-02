c9bafd969a7a7d120249c9b126a579de9a6cfddf
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2026-02-02 16:29:32 +0100

    feat : build script

---
+++ dotnet/build.sh
@@ -0,0 +1,5 @@
+
+docker build -t chisel -f Dockerfile .
+docker build -t chisel-curl -f Dockerfile.curl .
+docker build -t chainguard -f Dockerfile.chainguard .
+docker build -t dhi -f Dockerfile.dhi .
